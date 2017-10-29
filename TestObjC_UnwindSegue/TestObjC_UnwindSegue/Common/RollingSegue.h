d1a4548765c638c8e7b92019e4c952193175e80b
Toshiya Yoshida <kagilinn@hotmail.com>
2017-10-29 23:10:35 +0900

    カスタムSegue作成.

---
+++ TestObjC_UnwindSegue/TestObjC_UnwindSegue/Common/RollingSegue.h
@@ -0,0 +1,13 @@
+//
+//  RollingSegue.h
+//  TestObjC_UnwindSegue
+//
+//  Created by Toshiya Yoshida on 2017/10/29.
+//  Copyright © 2017年 kagilinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RollingSegue : UIStoryboardSegue
+
+@end
---
+++ TestObjC_UnwindSegue/TestObjC_UnwindSegue/Common/RollingSegue.m
@@ -0,0 +1,50 @@
+//
+//  RollingSegue.m
+//  TestObjC_UnwindSegue
+//
+//  Created by Toshiya Yoshida on 2017/10/29.
+//  Copyright © 2017年 kagilinn. All rights reserved.
+//
+
+#import "RollingSegue.h"
+
+@implementation RollingSegue
+
+- (instancetype)initWithIdentifier:(NSString *)identifier
+							source:(UIViewController *)source
+					   destination:(UIViewController *)destination
+{
+	self = [super initWithIdentifier:identifier
+							  source:source
+						 destination:destination];
+	return self;
+}
+
+- (void)perform {
+	__weak typeof(self) weak_self = self;
+	CGSize size = UIScreen.mainScreen.bounds.size;
+	[UIApplication.sharedApplication.keyWindow
+		addSubview:self.destinationViewController.view];
+	self.destinationViewController.view.frame
+		= CGRectMake(size.width, 0, size.width, size.height);
+	[UIView animateWithDuration:0.375f
+						  delay:0
+						options:UIViewAnimationOptionCurveEaseInOut
+		animations:^{
+			UIView *src  = weak_self.     sourceViewController.view;
+			UIView *dest = weak_self.destinationViewController.view;
+			dest.frame = CGRectMake(0, 0, size.width, size.height);
+			src .frame = CGRectOffset(dest.frame, -size.width, 0);
+		} completion:^(BOOL finished) {
+			@autoreleasepool {
+				UIViewController *src  = weak_self.sourceViewController;
+				UIViewController *dest = weak_self.destinationViewController;
+				[src presentViewController:dest
+								  animated:NO
+								completion:nil];
+			}
+		}
+	];
+}
+
+@end
